e7c974181a9fddb6b839409a40c474ccbeea31a6
Eugene Kim <eugenekim.edi@gmail.com>
2020-11-04 13:39:10 +0000

    Add router/mongodb

--- server/server.js
+++ server/server.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const gamesRouter = require('./helpers/games_router.js');
+const MongoClient = require('mongodb').MongoClient;
+
+
+app.use(bodyParser.json());
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('chess');
+    const gamesCollection = db.collection('games');
+    app.use('/api/games', gamesRouter(gamesCollection));
+})
+  .catch(console.error);
 
 app.listen(3000, function () {
     console.log(`Listening on port ${ this.address().port }`);
